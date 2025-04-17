23acc00d9503a6af7ea986bf0e8acd95d43ad2a0
ProgramEze <141289604+ProgramEze@users.noreply.github.com>
2025-04-17 15:02:27 -0300

    DB update

---
+++ gestion_eventos (sin datos).sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-12-2024 a las 19:59:11
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestion_eventos`
+--
+CREATE DATABASE IF NOT EXISTS `gestion_eventos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gestion_eventos`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistente`
+--
+
+CREATE TABLE `asistente` (
+  `idAsistente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` varchar(15) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `idEvento` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `fecha` date NOT NULL,
+  `ubicacion` varchar(50) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `participacion`
+--
+
+CREATE TABLE `participacion` (
+  `idParticipacion` int(11) NOT NULL,
+  `idAsistente` int(11) NOT NULL,
+  `idEvento` int(11) NOT NULL,
+  `confirmacion` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asistente`
+--
+ALTER TABLE `asistente`
+  ADD PRIMARY KEY (`idAsistente`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`idEvento`);
+
+--
+-- Indices de la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  ADD PRIMARY KEY (`idParticipacion`),
+  ADD UNIQUE KEY `idAsistente` (`idAsistente`,`idEvento`),
+  ADD UNIQUE KEY `participacion_id_asistente_id_evento` (`idAsistente`,`idEvento`),
+  ADD KEY `idEvento` (`idEvento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asistente`
+--
+ALTER TABLE `asistente`
+  MODIFY `idAsistente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  MODIFY `idParticipacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `participacion`
+--
+ALTER TABLE `participacion`
+  ADD CONSTRAINT `participacion_ibfk_1` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`),
+  ADD CONSTRAINT `participacion_ibfk_2` FOREIGN KEY (`idAsistente`) REFERENCES `asistente` (`idAsistente`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
